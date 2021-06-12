a3620445a96fa6918c3c4ef2e98606b5a5b19d4e
little077 <1837687575@qq.com>
2021-06-12 20:18:23 +0800

    第一次提交“

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item,index) in recommends" :key="index" class="recommend-item">
+        <a :href="item.link">
+            <img :src="item.image" alt="">
+            <div>{{item.title}}</div>
+        </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'RecommendView',
+    props:{
+        recommends:{
+            type:Array,
+            dafault(){
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style>
+ .recommend{
+     display: flex;
+     text-align: center;
+     font-size: 12px;
+     padding: 10px 0 20px;
+     border-bottom: 8px solid #eee;
+     width:100%;
+ }
+ .recommend-item{
+     flex:1
+ }
+ .recommend-item img{
+     width: 70px;
+     height: 70px;
+     
+     margin-bottom: 10px;
+ }
+</style>
