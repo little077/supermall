d558f365f3c45596f557731e0fbc729b61612677
little077 <1837687575@qq.com>
2021-08-12 16:47:31 +0800

    goods展示

--- src/network/home.js
+++ src/network/home.js
@@ -5,3 +5,12 @@ export function getHomeMultidata(){
         url:'/home/multidata'
     })
 }
+export function GetHomeGoods(type,page){
+    return request({
+        url:'/home/data',
+        params:{
+            type,
+            page
+        }
+    })
+}
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,159 +4,11 @@
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
     <feature-view />
-    <tab-control :titles="['流行','新款','精选']" class="tab-control" />
-   <ul>
-     <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li>
-      <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li>
-      <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li>
-      <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li>
-      <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li>
-      <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li>
+    <tab-control :titles="['流行','新款','精选']"
+     @tabClick='tabClick'
+     class="tab-control" />
+    <goods-list :goods='showGoods' />
 
-      <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li>
-      <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li>
-      <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li>v <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li> <li>1</li>
-     <li>2</li>
-     <li>3</li>
-     <li>4</li>
-     <li>5</li>
-     <li>6</li>
-   </ul>
     </div>
 </template>
 
@@ -166,8 +18,8 @@ import HomeSwiper from "./childComps/HomeSwiper"
 import RecommendView from './childComps/RecommendView'
 import FeatureView from "./childComps/FeatureView"
 import TabControl from "components/content/tabControl/TabControl.vue"
-import {getHomeMultidata} from "network/home"
-
+import {getHomeMultidata,GetHomeGoods} from "network/home"
+import GoodsList from 'components/content/goods/GoodsList'
   export default {
    name:"Home",
    components:{
@@ -175,22 +27,69 @@ import {getHomeMultidata} from "network/home"
      HomeSwiper,
      RecommendView,
      FeatureView,
-     TabControl
+     TabControl,
+     GoodsList
+   },
+   computed:{
+     showGoods(){
+       return this.goods[this.currentType].list
+     }
    },
    data(){
  
      return {
        banners:[],
-       recommends:[]
+       recommends:[],
+       goods:{
+         'pop':{page:0,list:[]},
+         'new':{page:0,list:[]},
+         'sell':{page:0,list:[]},
+       },
+       currentType:'pop'
      }
    },
     created(){
-      getHomeMultidata().then(res=>
+      //具体的逻辑代码放在methods里面
+      //请求多个数据
+      this.getHomeMultidata(),
+      //请求商品数据
+      this.GetHomeGoods('pop')
+      this.GetHomeGoods('new')
+      this.GetHomeGoods('sell')
+       
+    },
+    methods:{ 
+      /***
+       * 事件监听方法
+       */
+      tabClick(index){
+        if(index==0){
+          this.currentType='pop'
+        }
+        else if(index==1){
+          this.currentType='new'
+        }
+        else{
+          this.currentType='sell'
+        }
+      },
+      /**
+       * 网络请求方法
+       */
+      getHomeMultidata(){  getHomeMultidata().then(res=>
         {
           this.banners=res.data.banner.list
           this.recommends=res.data.recommend.list
-        })
+        })},
+      GetHomeGoods(type){
+        const page = this.goods[type].page+1
+         GetHomeGoods(type,page).then(res=>{
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page+=1} 
+         )
+      }
     }
+   
   }
 </script>
 
@@ -211,5 +110,6 @@ import {getHomeMultidata} from "network/home"
 .tab-control{
   position: sticky;
   top:44px;
+  z-index: 9;
 }
 </style>
