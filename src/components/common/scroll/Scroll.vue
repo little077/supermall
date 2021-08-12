b189ec6572a756391cc68aa1c81d6850f063bf6b
little077 <1837687575@qq.com>
2021-08-12 22:55:25 +0800

    下拉加载更多完成

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+        <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import BScroll from '@better-scroll/core'
+import ObserveImage from '@better-scroll/observe-image'
+import Pullup from '@better-scroll/pull-up'
+BScroll.use(ObserveImage)
+BScroll.use(Pullup)
+export default {
+  name:"Scroll",
+  props:{
+      probeType:{
+          type:Number,
+          default:0
+      },
+      pullUpLoad:{
+          type:Boolean,
+          default:false
+      }
+  },
+  data(){
+      return {
+          scroll:null,
+          
+      }
+  },
+  methods:{
+    backTop(x,y,time=300){
+        this.scroll.scrollTo(x,y,time)
+    },
+    finishPullUp(){
+        this.scroll.finishPullUp()
+    }
+  },
+  mounted(){
+      this.scroll=new BScroll(this.$refs.wrapper,{
+        click:true,
+        observeImage: true,
+        probeType:this.probeType,
+        pullUpLoad:this.pullUpLoad
+  })
+  this.scroll.on('scroll',(position)=>{
+    this.$emit('scroll',position)
+  })
+  this.scroll.on('pullingUp',()=>{
+      this.$emit('pullingUp')
+  })
+
+  }
+  
+  
+}
+
+</script>
+
+<style>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,7 +1,14 @@
 <template>
 <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"/>
+    <scroll class="content"
+     ref="backTo" 
+     :probe-type='3'
+      @scroll="contentScroll"
+      :pull-up-load="true"
+      @pullingUp='loadMore'
+    >
+              <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
     <feature-view />
     <tab-control :titles="['流行','新款','精选']"
@@ -9,6 +16,8 @@
      class="tab-control" />
     <goods-list :goods='showGoods' />
 
+    </scroll>
+    <back-top @click.native="backTop" v-show="isShowBackTop" />
     </div>
 </template>
 
@@ -20,6 +29,9 @@ import FeatureView from "./childComps/FeatureView"
 import TabControl from "components/content/tabControl/TabControl.vue"
 import {getHomeMultidata,GetHomeGoods} from "network/home"
 import GoodsList from 'components/content/goods/GoodsList'
+import Scroll from 'components/common/scroll/Scroll.vue'
+import BackTop from 'components/content/backTop/BackTop'
+
   export default {
    name:"Home",
    components:{
@@ -28,7 +40,10 @@ import GoodsList from 'components/content/goods/GoodsList'
      RecommendView,
      FeatureView,
      TabControl,
-     GoodsList
+     GoodsList,
+     Scroll,
+     BackTop
+
    },
    computed:{
      showGoods(){
@@ -45,7 +60,8 @@ import GoodsList from 'components/content/goods/GoodsList'
          'new':{page:0,list:[]},
          'sell':{page:0,list:[]},
        },
-       currentType:'pop'
+       currentType:'pop',
+       isShowBackTop:false
      }
    },
     created(){
@@ -59,9 +75,29 @@ import GoodsList from 'components/content/goods/GoodsList'
        
     },
     methods:{ 
+      /**
+       * 下拉加载更多
+       */
+      loadMore(){
+        this.GetHomeGoods(this.currentType)
+      },
       /***
        * 事件监听方法
        */
+      contentScroll(position){
+        if(position.y<=-1000){
+          this.isShowBackTop=true
+        }
+        else{
+          this.isShowBackTop=false
+        }
+
+      },
+      //回到顶部，父组件使用.native监听事件使用子组件中的方法
+      backTop(){
+        this.$refs.backTo.backTop(0,0)
+      },
+      //切换选框
       tabClick(index){
         if(index==0){
           this.currentType='pop'
@@ -85,7 +121,8 @@ import GoodsList from 'components/content/goods/GoodsList'
         const page = this.goods[type].page+1
          GetHomeGoods(type,page).then(res=>{
           this.goods[type].list.push(...res.data.list)
-          this.goods[type].page+=1} 
+          this.goods[type].page+=1
+          this.$refs.backTo.finishPullUp()} 
          )
       }
     }
@@ -96,6 +133,9 @@ import GoodsList from 'components/content/goods/GoodsList'
 <style scoped>
 #home{
   padding-top: 44px;
+  position: relative;
+  height: 100vh;
+  /* 百分之百视口*/
 }
 .home-nav{
   background-color: var(--color-tint);
@@ -111,5 +151,13 @@ import GoodsList from 'components/content/goods/GoodsList'
   position: sticky;
   top:44px;
   z-index: 9;
+}
+.content{
+  overflow: hidden;
+  position: absolute;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+  top:44px;
 }
 </style>
